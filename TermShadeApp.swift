497e905e4a04e60dbcaf32d011f742c373869543
emiyl <me@emiyl.com>
2026-04-04 20:37:08 +0100

    Initial Commit

---
+++ TermShadeApp.swift
@@ -0,0 +1,15 @@
+//
+//  TermShadeApp.swift
+//  TermShade
+//
+
+import SwiftUI
+
+@main
+struct TermShadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
